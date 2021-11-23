0f7de3b4e1c99d2c19492c94dbd99ed15df8eec2
Anthony Rappa <rappa819@gmail.com>
2021-11-23 10:45:17 -0500

    Add unique keys

--- resources/views/datatable.blade.php
+++ resources/views/datatable.blade.php
@@ -13,8 +13,9 @@
         ]"
     >
         <x-slot name="thead">
-            @foreach($columns as $column)
+            @foreach($columns as $index => $column)
                 <x-livewire-tables::table.th
+                    wire:key="header-col-{{ $index }}-{{ $this->id }}"
                     :theme="$this->getTheme()"
                     :column="$column"
                     :customAttributes="$this->getThAttributes($column)"
@@ -22,15 +23,16 @@
             @endforeach
         </x-slot>
 
-        @forelse ($rows as $index => $row)
+        @forelse ($rows as $rowIndex => $row)
             <x-livewire-tables::table.tr
-                wire:key="row-{{ $index }}-{{ $this->id }}"
+                wire:key="row-{{ $rowIndex }}-{{ $this->id }}"
                 :theme="$this->getTheme()"
                 :index="$index"
                 :customAttributes="$this->getTrAttributes($row, $index)"
             >
-                @foreach($columns as $column)
+                @foreach($columns as $colIndex => $column)
                     <x-livewire-tables::table.td
+                        wire:key="cell-{{ $rowIndex }}-{{ $colIndex }}-{{ $this->id }}"
                         :theme="$this->getTheme()"
                         :customAttributes="$this->getTdAttributes($column, $row, $index)"
                     >
@@ -40,6 +42,7 @@
             </x-livewire-tables::table.tr>
         @empty
             <x-livewire-tables::table.empty
+                wire:key="empty-message-{{ $this->id }}"
                 :theme="$this->getTheme()"
                 :colspan="count($columns)"
                 :message="$this->getEmptyMessage()"
