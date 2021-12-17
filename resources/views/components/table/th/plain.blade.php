fbe9fe1bbef27efd6d76e4315f9d0cb7701ab178
Anthony Rappa <rappa819@gmail.com>
2021-12-17 00:30:42 -0500

    Update

--- resources/views/components/table/th/plain.blade.php
+++ resources/views/components/table/th/plain.blade.php
@@ -5,7 +5,7 @@
 @endphp
 
 @if ($theme === 'tailwind')
-    <th scope="col" {{ $attributes->merge(['class' => 'table-cell px-3 py-2 md:px-6 md:py-3 text-left bg-gray-50 dark:bg-gray-800']) }}>{{ $slot }}</th>
+    <th scope="col" {{ $attributes->merge(['class' => 'table-cell px-3 py-2 md:px-6 md:py-3 text-center sm:text-left bg-gray-50 dark:bg-gray-800']) }}>{{ $slot }}</th>
 @elseif ($theme === 'bootstrap-4')
 
 @elseif ($theme === 'bootstrap-5')
